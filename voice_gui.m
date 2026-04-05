1ea11b6d55d29b74b28b68c2bddc8ba592a24b7d
chengmiao2005 <chengmiao2005@icloud.com>
2026-04-05 15:21:53 +0800

    Update voice_gui.m

--- voice_gui.m
+++ voice_gui.m
@@ -1,3 +1,4 @@
+ % GUI 初始化代码（由 GUIDE 自动生成）
 function varargout = voice_gui(varargin)
 gui_Singleton = 1;
 gui_State = struct('gui_Name', mfilename, 'gui_Singleton', gui_Singleton, ...
@@ -6,6 +7,8 @@ gui_State = struct('gui_Name', mfilename, 'gui_Singleton', gui_Singleton, ...
 if nargin && ischar(varargin{1}), gui_State.gui_Callback = str2func(varargin{1}); end
 if nargout, [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
 else, gui_mainfcn(gui_State, varargin{:}); end
+
+
 
 function voice_gui_OpeningFcn(hObject, ~, handles, ~)
 handles.output = hObject;
